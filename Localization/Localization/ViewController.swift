1ae1f35c249a5670bc686893765d26d0b458416f
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-17 12:04:23 +0200

    OnGoing

--- Localization/Localization/ViewController.swift
+++ Localization/Localization/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHello: UILabel!
+    
+    @IBOutlet weak var btnchangeLanguage: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lblHello.text = NSLocalizedString("Hello", comment: "Hello Label")
+        btnchangeLanguage.setTitle(NSLocalizedString("ChangeLanguage", comment: ""), for: .normal)
+       
     }
 
 
+    @IBAction func btnSwitchLanguage(_ sender: UIButton) {
+        let currentLang = Locale.current.language.languageCode
+        print(currentLang ?? "")
+        let newLanguage = currentLang == "en" ? "fr" : "en"
+        UserDefaults.standard.setValue([newLanguage], forKey: "AppleLanguages")
+        exit(0)
+    }
 }
 
